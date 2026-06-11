26b9f8421a713f244f848004ebef50fe0deadaa7
weizx <weizxemail@gmail.com>
2026-06-11 11:18:40 +0800

    完成两种发射子弹模式

---
+++ assets/Scripts/Bullet.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from "cc";
+const { ccclass, property } = _decorator;
+
+@ccclass("Bullet")
+export class Bullet extends Component {
+  @property
+  speed: number = 500;
+
+  update(deltaTime: number) {
+    const pos = this.node.position;
+    const s = this.speed * deltaTime;
+    this.node.setPosition(pos.x, pos.y + s);
+  }
+}
--- assets/Scripts/Player.ts
+++ assets/Scripts/Player.ts
@@ -4,15 +4,59 @@ import {
   EventTouch,
   Input,
   input,
+  instantiate,
   Node,
+  Prefab,
   Vec3,
 } from "cc";
 const { ccclass, property } = _decorator;
 
+enum ShootType {
+  OneShoot,
+  TwoShoot,
+}
+
 @ccclass("Player")
 export class Player extends Component {
+  @property
+  shootRate: number = 0.3;
+  shootTimer: number = 0;
+
+  @property(Prefab)
+  bullet1Prefa: Prefab = null;
+  @property(Prefab)
+  bullet2Prefa: Prefab = null;
+  @property(Prefab)
+  bullet3Prefa: Prefab = null;
+
+  @property(Node)
+  bullet1Pos: Node = null;
+  @property(Node)
+  bullet2Pos: Node = null;
+  @property(Node)
+  bullet3Pos: Node = null;
+
+  @property(Node)
+  bulletParent: Node = null;
+
+  @property(typeof ShootType)
+  shootType: ShootType = ShootType.OneShoot;
+
   protected onLoad(): void {
     input.on(Input.EventType.TOUCH_MOVE, this.onTouchMove, this);
+  }
+
+  protected update(dt: number): void {
+    switch (this.shootType) {
+      case ShootType.OneShoot:
+        this.shootRate = 0.3;
+        this.oneShoot(dt);
+        break;
+      case ShootType.TwoShoot:
+        this.shootRate = 0.2;
+        this.twoShoot(dt);
+        break;
+    }
   }
 
   protected onDestroy(): void {
@@ -37,5 +81,28 @@ export class Player extends Component {
       targetPos.y = -380;
     }
     this.node.setPosition(targetPos);
+  }
+
+  oneShoot(dt: number) {
+    this.shootTimer += dt;
+    if (this.shootTimer > this.shootRate) {
+      this.shootTimer = 0;
+      const bullet1 = instantiate(this.bullet1Prefa);
+      this.bulletParent.addChild(bullet1);
+      bullet1.setWorldPosition(this.bullet1Pos.getWorldPosition());
+    }
+  }
+
+  twoShoot(dt: number) {
+    this.shootTimer += dt;
+    if (this.shootTimer > this.shootRate) {
+      this.shootTimer = 0;
+      const bullet2 = instantiate(this.bullet2Prefa);
+      const bullet3 = instantiate(this.bullet3Prefa);
+      this.bulletParent.addChild(bullet2);
+      this.bulletParent.addChild(bullet3);
+      bullet2.setWorldPosition(this.bullet2Pos.getWorldPosition());
+      bullet3.setWorldPosition(this.bullet3Pos.getWorldPosition());
+    }
   }
 }
