d2aedf51caa8a15aa69b17cc23bdc72900601d00
weizx <weizxemail@gmail.com>
2026-06-11 10:04:23 +0800

    添加背景动画

---
+++ assets/Scripts/Bg.ts
@@ -0,0 +1,25 @@
+import { _decorator, Component, Node } from "cc";
+const { ccclass, property } = _decorator;
+
+@ccclass("Bg")
+export class Bg extends Component {
+  @property(Node)
+  bg01: Node = null;
+  @property(Node)
+  bg02: Node = null;
+  @property
+  speed: number = 100;
+
+  start() {}
+
+  update(deltaTime: number) {
+    this.bg01.y -= this.speed * deltaTime;
+    this.bg02.y -= this.speed * deltaTime;
+    if (this.bg01.y < -852) {
+      this.bg01.y += 852 * 2;
+    }
+    if (this.bg02.y < -852) {
+      this.bg02.y += 852 * 2;
+    }
+  }
+}
--- assets/Scripts/StatGame.ts
+++ assets/Scripts/StatGame.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node } from "cc";
+import { _decorator, Component, director, Node } from "cc";
 const { ccclass, property } = _decorator;
 
 @ccclass("StatGame")
@@ -7,5 +7,7 @@ export class StatGame extends Component {
 
   update(deltaTime: number) {}
 
-  startGame() {}
+  startGame() {
+    director.loadScene("Game");
+  }
 }
