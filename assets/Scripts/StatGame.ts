4f047a9984bc54091bb5260ca4a28934a0612ada
weizx <weizxemail@gmail.com>
2026-06-11 09:40:43 +0800

    创建开始场景

---
+++ assets/Scripts/StatGame.ts
@@ -0,0 +1,11 @@
+import { _decorator, Component, Node } from "cc";
+const { ccclass, property } = _decorator;
+
+@ccclass("StatGame")
+export class StatGame extends Component {
+  start() {}
+
+  update(deltaTime: number) {}
+
+  startGame() {}
+}
